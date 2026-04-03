3a83655b9c75e7b5cd2baa7c7cd8322976018bdb
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2026-04-03 08:29:36 -0500

    Show how to make custom models for x any labeling

--- customModel/modeltoOnnx.py
+++ customModel/modeltoOnnx.py
@@ -5,6 +5,6 @@ model = YOLO("yolo11m_4500_imgsz1600_b1_2024-01-18.pt")
 
 # Export to ONNX with static shapes
 # imgsz should match the size you used during training (e.g., 640)
-path = model.export(format="onnx", imgsz=640, dynamic=False, opset=12)
+path = model.export(format="onnx", imgsz=1600, dynamic=False, opset=12)
 
 print(f"Model exported to: {path}")
